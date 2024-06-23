28bbe14722035eac01732b1f575d85838c374e6a
thedev-junyoung <thedev.junyoung@gmail.com>
2024-06-23 14:46:37 +0900

    'createdAt' 필드 Data 타입 LocalDateTime 으로 변경

--- src/main/java/com/example/springboilerplate/dto/user/UserDTO.java
+++ src/main/java/com/example/springboilerplate/dto/user/UserDTO.java
@@ -3,6 +3,7 @@ package com.example.springboilerplate.dto.user;
 import com.example.springboilerplate.type.UserRole;
 import lombok.Data;
 
+import java.time.LocalDateTime;
 import java.util.Date;
 
 @Data
@@ -12,5 +13,8 @@ public class UserDTO {
     String username;
     UserRole role;
     String profileImg;
-    Date createdAt;
+    LocalDateTime createdAt; // LocalDateTime으로 변경
+    // LocalDateTime은 날짜와 시간을 모두 포함하는 클래스
+    // Date 클래스는 날짜와 시간을 모두 포함하지만, Date 클래스는 날짜와 시간을 나누어서 사용할 수 없음
+    // LocalDateTime 클래스는 날짜와 시간을 나누어서 사용할 수 있음
 }
